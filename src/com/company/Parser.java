870664b0d7890a0a7e7a7be4dec40cd89136413f
Magnusn86 <mn@simnet.is>
2017-02-07 16:27:37 +0000

    added nextToken() fucntion

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -1,10 +1,26 @@
 /**
  * Created by Owner on 05-Feb-17.
+ * Statements → Statement ; Statements | end
+ * Statement ⇒ id = Expr | print id
+ * Expr → Term | Term + Expr | Term – Expr
+ * Term ⇒ Factor | Factor * Term
+ * Factor → int | id | ( Expr )
  */
 public class Parser {
 
 
     public Parser(Lexer lexer) {
+        this.checkLexer(lexer);
+    }
+
+
+    private void checkLexer(Lexer lex) {
+
+        for (int i = 0; i < 10; i++) {
+            Token test = lex.nextToken();
+            System.out.println("Lex: " + test.lexeme + " , Token: " + test.tCode);
+        }
 
     }
 }
+
